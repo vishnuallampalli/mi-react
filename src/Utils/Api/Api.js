0bd49e0481c5131fbe167de2893b39311055461f
sandeepkumarreddymallidi <106801934+sandeepkumarreddymallidi@users.noreply.github.com>
2023-05-29 23:42:37 +0530

    initial commit

---
+++ src/Utils/Api/Api.js
@@ -0,0 +1,9 @@
+
+
+//local api
+ export const baseURL ='http://localhost:8080/'
+// Server Api
+// export const baseURL ='https://ml-hospital-mngmt.azurewebsites.net/'
+//vm Api
+//export const baseURL ='http://172.174.209.218:8080/'
+//export const baseURL ='https://f577-182-76-136-42.in.ngrok.io/'
