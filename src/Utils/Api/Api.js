e0dbd92e15519ac1d36a2980bbc635fb3db561e6
wahtej <125274697+wahtej@users.noreply.github.com>
2023-05-31 23:19:15 +0530

    Update Api.js

--- src/Utils/Api/Api.js
+++ src/Utils/Api/Api.js
@@ -1,7 +1,7 @@
 
 
 //local api
- export const baseURL ='http://backendservice.default.svc.cluster.local:31502/'
+ export const baseURL ='http://13.126.135.9:31502/'
 // Server Api
 // export const baseURL ='https://ml-hospital-mngmt.azurewebsites.net/'
 //vm Api
