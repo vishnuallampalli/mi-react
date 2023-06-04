ec11b574bb267d572bc25b3f6f9ff2b0d603f234
vishnuallampalli <112451766+vishnuallampalli@users.noreply.github.com>
2023-06-04 23:53:32 +0530

    Update Api.js

--- src/Utils/Api/Api.js
+++ src/Utils/Api/Api.js
@@ -1,7 +1,7 @@
 
 
 //local api
- export const baseURL ='http://backendservice.default.svc.cluster.local:31502/'
+ export const baseURL ='http://65.0.92.50:31502/'
 // Server Api
 // export const baseURL ='https://ml-hospital-mngmt.azurewebsites.net/'
 //vm Api
